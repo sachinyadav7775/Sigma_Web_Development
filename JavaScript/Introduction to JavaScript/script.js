bdbe4a5e0c4e46d6479f9bc8c8a7b8e3f6deb927
sachin yadav <sachinyadav63928@gmail.com>
2026-01-11 21:42:55 +0530

    add new file

---
+++ JavaScript/Introduction to JavaScript/script.js
@@ -0,0 +1,6 @@
+alert("Hello Sachin");
+console.log("code is running...");
+console.log("code is also running...");
+console.log("code is looking like a wow...");
+var a = prompt("Enter Your Number");
+console.log("Your Number is " + a);
