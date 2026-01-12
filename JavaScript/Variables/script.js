252ebd794b0e6a7ebc198ae6eb70ddc2f81e7642
sachin yadav <sachinyadav63928@gmail.com>
2026-01-12 07:28:38 +0530

    add new file

---
+++ JavaScript/Variables/script.js
@@ -0,0 +1,22 @@
+console.log("Hey this i am sky")
+let a = 4;
+let b = 5;
+let c = "sachin";
+console.log(a + b + 4)
+console.log(typeof a, typeof b, typeof c);
+
+let x = "sachin";
+let y = 22;
+let z = 3.33;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name : "sachin",
+    "job code" : "20core",
+    "is_handsome" : true
+}
+console.log(o);
