09e099711ae46f282dc8123e92bf48ca37dc0374
sachin yadav <sachinyadav63928@gmail.com>
2026-01-12 19:33:26 +0530

    add new file

---
+++ JavaScript/functions/script.js
@@ -0,0 +1,10 @@
+function nice(name) {
+    console.log(" Hey " + name + " you aur nice!")
+    console.log(" Hey " + name + " you aur good!")
+    console.log(" Hey " + name + " your shirt is nice!")
+    console.log(" Hey " + name + " your course is good too!") 
+}
+
+nice("sujeet");
+nice("anurag");
+nice("Shivam");
