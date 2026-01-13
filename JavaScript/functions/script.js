de51de56c81248bd745d8f2fafdbfb67b2c089e3
sachin yadav <sachinyadav63928@gmail.com>
2026-01-13 07:03:48 +0530

    add new paragraph

--- JavaScript/functions/script.js
+++ JavaScript/functions/script.js
@@ -8,3 +8,22 @@ function nice(name) {
 nice("sujeet");
 nice("anurag");
 nice("Shivam");
+
+function sum(a, b, c = 8) {
+    // console.log(a + b + c);
+    // console.log(typeof a, typeof b, typeof c)
+    return (a + b + c);
+}
+
+result1 = sum(3,4);
+result2 = sum(22,42);
+console.log("The sum of these numbers is: ", result1)
+console.log("The sum of these numbers is: ", result2)
+
+const func1 = (x)=> {
+    console.log("I am an arrow function", x)
+}
+
+func1(44);
+func1(55);
+func1(77);
