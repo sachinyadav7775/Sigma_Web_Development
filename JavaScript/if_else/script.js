1c084b8c226692dcdc58b8b68bde8be74244b278
sachin yadav <sachinyadav63928@gmail.com>
2026-01-13 21:51:22 +0530

    add new paragraph

--- JavaScript/if_else/script.js
+++ JavaScript/if_else/script.js
@@ -4,10 +4,20 @@ let age = 17;
 
 if(age >= 18) {
     console.log("You can drive");
-}
-else if(age > 1 && age <=12 ) {
+} else if(age > 1 && age <=12 ) {
     console.log("You are small child not drive")
-}
-else {
+} else {
     console.log("You can not drive");
+}
+
+let x = 10;
+
+if (x > 15) {
+    console.log("x is greater than 15");
+} else if (x > 10) {
+    console.log("x is greater than 10 but less than or equal to 15");
+} else if (x > 5) {
+    console.log("x is greater than 5 but less than or equal to 10");
+} else {
+    console.log("x is less than or equal to 5");
 }
