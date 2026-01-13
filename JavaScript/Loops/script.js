4ac67c5fa3ea5039d4e7549215218c316957ad37
sachin yadav <sachinyadav63928@gmail.com>
2026-01-13 19:28:47 +0530

    add new file

---
+++ JavaScript/Loops/script.js
@@ -0,0 +1,12 @@
+console.log("Hey i am loops");
+
+let n = 1;
+
+// for (let i=0; i<100; i++) {
+//     console.log(n + i);
+// }
+
+while(n <= 50) {
+    console.log(n);
+    n++;
+}
---
+++ JavaScript/if_else/script.js
@@ -0,0 +1,13 @@
+console.log("Hey i am if else")
+
+let age = 17;
+
+if(age >= 18) {
+    console.log("You can drive");
+}
+else if(age > 1 && age <=12 ) {
+    console.log("You are small child not drive")
+}
+else {
+    console.log("You can not drive");
+}
