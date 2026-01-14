bf78a6a4021705350188a41030b07c6d2eefbbee
sachin yadav <sachinyadav63928@gmail.com>
2026-01-14 06:36:07 +0530

    add new paragraph

--- JavaScript/if_else/script.js
+++ JavaScript/if_else/script.js
@@ -5,7 +5,7 @@ let age = 17;
 if(age >= 18) {
     console.log("You can drive");
 } else if(age > 1 && age <=12 ) {
-    console.log("You are small child not drive")
+    console.log("You are small child not drive");
 } else {
     console.log("You can not drive");
 }
@@ -21,3 +21,25 @@ if (x > 15) {
 } else {
     console.log("x is less than or equal to 5");
 }
+
+let Marks = 38;
+
+if(Marks > 90 && Marks <= 100) {
+    console.log("A1");
+} else if(Marks > 80  && Marks <= 90) {
+    console.log("A2");
+} else if(Marks > 70 && Marks <= 80) {
+    console.log("B1")
+} else if(Marks > 60 && Marks <= 70) {
+    console.log("B2")
+} else if(Marks > 50 && Marks <= 60) {
+    console.log("C1")
+} else if(Marks > 40 && Marks <= 50) {
+    console.log("C2")
+} else if(Marks > 30 && Marks <= 40) {
+    console.log("D");
+} else if(Marks >= 23) {
+    console.log("Pass")
+} else {
+    console.log("Fail")
+}  
