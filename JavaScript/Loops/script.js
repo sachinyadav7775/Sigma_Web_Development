4eb962e96ec59df7733475454b08ad75e99f239e
sachin yadav <sachinyadav63928@gmail.com>
2026-01-15 20:53:02 +0530

    add new paragraph

--- JavaScript/Loops/script.js
+++ JavaScript/Loops/script.js
@@ -1,10 +1,25 @@
-console.log("Hey i am loops");
+console.log("Hey i am a tutorial on loops");
 
-let n = 1;
+// let n = 1;
 
 // for (let i=0; i<100; i++) {
 //     console.log(n + i);
 // }
+
+let object = {
+    name: "Sachin",
+    role: "programmer",
+    company: "pw AI",
+}
+
+for (const key in object) {
+    const element = object[key];
+    console.log(key,element)
+}
+
+for (const c of "sachin") {
+    console.log(c)
+}
 
 while(n <= 50) {
     console.log(n);
---
+++ JavaScript/Loops/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for (const key in object) {
+//     const element = object[key];
+//     console.log(key,element)
+// }
