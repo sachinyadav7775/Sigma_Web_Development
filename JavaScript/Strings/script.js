35ba0cc42c6eea487fbf09f808957501c35a18b5
sachin yadav <sachinyadav63928@gmail.com>
2026-01-16 13:28:18 +0530

    add new file

---
+++ JavaScript/Strings/script.js
@@ -0,0 +1,27 @@
+console.log("This is  Strings tutorial")
+let a = "sachin";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+// console.log(a[6])
+console.log(a.length)
+
+let real_name = "Sachin"
+let friend = "Sujeet"
+
+console.log("His name is " + real_name + " and his friend name is " + friend)
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "Anurag"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+console.log(b.slice(3))
+
+let c = "Shivam bhai "
+console.log(c.replace("bhai", "bhau"))
+console.log(c.concat(a, " Sahil", " Avanish"))
