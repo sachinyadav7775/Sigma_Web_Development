1b7eaeb2c5e42a68635704718ca9a55655358113
sachin yadav <sachinyadav63928@gmail.com>
2026-01-19 12:43:10 +0530

    add new file

---
+++ JavaScript/DOM/script.js
@@ -0,0 +1,21 @@
+console.log("Hello World")
+document.body
+document.body.firstChild
+document.body.firstChild
+document.body.childNodes
+document.body.childNodes[0]
+document.body.childNodes[1]
+document.body.childNodes[3]
+document.body.childNodes[4]
+document.body.childNodes[1].childNodes[1]
+document.body.childNodes[1].childNodes[2]
+document.body.childNodes[1].childNodes[3]
+let cont = document.body.childNodes[1]
+cont.lastChild
+cont.firstChild
+cont.firstElementChild
+cont.lastElementChild
+cont.lastElementChild.style.color = "red";
+cont.lastElementChild.style.backgroundColor = "skyblue";
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
