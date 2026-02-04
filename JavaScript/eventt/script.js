f52d6ad098146fbf0258f4e5b0f1321f7ba1cd17
sachin yadav <sachinyadav63928@gmail.com>
2026-02-04 12:07:56 +0530

    add new file

---
+++ JavaScript/eventt/script.js
@@ -0,0 +1,17 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("dblclick", () =>{
+    document.querySelector(".box").innerHTML = "<b> Yes Button Click Me <b> Enjoy Sachin !"
+})
+
+button.addEventListener("contextmenu", () =>{
+    alert("Dont Right Click ")
+})
+
+button.addEventListener("keydown", (e)=>{
+    console.log(e)
+})
+
+// document.addEventListener("keydown", (e)=>{
+//     console.log(e.key,e.keyCode)
+// })
