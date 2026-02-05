6442f5a10ce9edab2407c37c728c30cbd144bc8f
sachin yadav <sachinyadav63928@gmail.com>
2026-02-05 11:06:42 +0530

    add new paragraph

--- JavaScript/Closures/script.js
+++ JavaScript/Closures/script.js
@@ -17,24 +17,68 @@
 // c = hello()
 // c()
 
-function Closures() {
-    const x = () => {
-        let a = 1
-        console.log(a)
-        const y = () => {
-            // let a = 2
-            console.log(a)
-            const z = () => {
-                // let a = 8
-                console.log(a)
-            }
-            z()
-        }
-        a = 477
-        y()
-    }
-    return x
-}
+// function Closures() {
+//     const x = () => {
+//         let a = 1
+//         console.log(a)
+//         const y = () => {
+//             // let a = 2
+//             console.log(a)
+//             const z = () => {
+//                 // let a = 8
+//                 console.log(a)
+//             }
+//             z()
+//         }
+//         a = 477
+//         y()
+//     }
+//     return x
+// }
 
-let a = Closures()
-a()
+// let a = Closures()
+// a()
+
+// function makeMultiplier() {
+//     let sum = 
+//     console.log("3*a")
+// }
+
+// function makeMultiplier(a) {
+//     return function(b) {
+//         return a * b;
+//     }
+// }
+
+// const double = makeMultiplier(2);
+// double(2); 
+
+// function createCounter() {
+//     let count = 0;
+
+//     return {
+//         increment: function () {
+//             count = count + 1
+//             console.log(count)
+//         },
+//         decrement: function () {
+//             count = count - 1
+//             console.log(count)
+//         }
+//     }
+// }
+
+
+// const c = createCounter();
+
+// c.increment(); // 1
+// c.increment(); // 2
+// c.decrement(); // 1
+
+for (var i = 1; i <= 5; i++) {
+    (function(x) {
+        setTimeout(function() {
+            console.log(x);
+        }, 1000);
+    })(i);
+}
