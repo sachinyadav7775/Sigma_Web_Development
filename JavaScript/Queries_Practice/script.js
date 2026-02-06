4d953ceb005c69e27f9ba730eb96450cdc3b31a6
sachin yadav <sachinyadav63928@gmail.com>
2026-02-06 11:58:26 +0530

    add new paragraph

---
+++ JavaScript/Queries_Practice/script.js
@@ -0,0 +1,25 @@
+// for(let i=1; i<=10; i++) {
+//     console.log(i)
+// }
+
+// let i = 1
+// while(i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+for(i=1; i<=20; i++) {
+    if(i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+// let i = 1;
+
+// while(i <= 20){
+
+//     if(i % 2 == 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
