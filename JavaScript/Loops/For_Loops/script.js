6cedc2d0711f1d42d29b75df460abfb7225f4737
sachin yadav <sachinyadav63928@gmail.com>
2026-02-06 12:29:15 +0530

    add new file name

--- JavaScript/Loops/For_Loops/script.js
+++ JavaScript/Loops/For_Loops/script.js
@@ -1,10 +1,23 @@
-console.log("Hey i am a tutorial on loops");
+console.log("Hey i am a tutorial on loops")
 
 // let n = 1;
-
 // for (let i=0; i<100; i++) {
-//     console.log(n + i);
+//     console.log(n + i)
 // }
+
+// 1 se 100 Tak Number Print
+let n = 10
+for(let i=1; i<=n; i++) {
+    console.log(i)
+}
+
+// Even Number Print
+let m = 20
+for(i=1; i<=m; i++) {
+    if(i % 2 == 0) {
+        console.log(i)
+    }
+}
 
 let object = {
     name: "Sachin",
@@ -19,9 +32,4 @@ for (const key in object) {
 
 for (const c of "sachin") {
     console.log(c)
-}
-
-while(n <= 50) {
-    console.log(n);
-    n++;
 }
---
+++ JavaScript/Loops/While_Loops/script.js
@@ -0,0 +1,16 @@
+// 1 se 100 Tak Number Print
+let n = 1
+while(n <= 10) {
+    console.log(n)
+    n++
+}
+
+// Even Number Print
+let m = 1;
+while(m <= 20){
+
+    if(m % 2 == 0) {
+        console.log(m);
+    }
+    m++;
+}
