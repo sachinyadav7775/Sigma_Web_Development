5c0163c1998471bde586250471c9c11ebca4592a
sachin yadav <sachinyadav63928@gmail.com>
2026-02-06 12:05:57 +0530

    add new file

---
+++ JavaScript/For_Loops/script.js
@@ -0,0 +1,11 @@
+// 1 se 100 Tak Number Print
+for(let i=1; i<=10; i++) {
+    console.log(i)
+}
+
+// Even Number Print
+for(i=1; i<=20; i++) {
+    if(i % 2 == 0) {
+        console.log(i)
+    }
+}
---
+++ JavaScript/While_Loops/script.js
@@ -0,0 +1,16 @@
+// 1 se 100 Tak Number Print
+let i = 1
+while(i <= 10) {
+    console.log(i)
+    i++
+}
+
+// Even Number Print
+let j = 1;
+while(j <= 20){
+
+    if(j % 2 == 0) {
+        console.log(j);
+    }
+    j++;
+}
