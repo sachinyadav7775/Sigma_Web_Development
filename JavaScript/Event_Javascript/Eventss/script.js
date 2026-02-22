a4ab14a87726e1b60bc9ef5dca875263d4361de9
sachin yadav <sachinyadav63928@gmail.com>
2026-02-22 06:59:02 +0530

    add new paragraph

--- JavaScript/Event_Javascript/Eventss/script.js
+++ JavaScript/Event_Javascript/Eventss/script.js
@@ -1,14 +1,14 @@
 let button = document.getElementById("btn")
 
-button.addEventListener("dblclick", () =>{
+button.addEventListener("dblclick", () => {
     document.querySelector(".box").innerHTML = "<b> Yes Button Click Me <b> Enjoy Sachin !"
 })
 
-button.addEventListener("contextmenu", () =>{
+button.addEventListener("contextmenu", () => {
     alert("Dont Right Click ")
 })
 
-button.addEventListener("keydown", (e)=>{
+button.addEventListener("keydown", (e)=> {
     console.log(e)
 })
 
